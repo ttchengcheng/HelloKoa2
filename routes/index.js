051289ff153fd1f0a46d4044cb5d562e6343cb31
程涛 <tchengim30@sina.com>
2017-10-17 21:36:31 +0800

    an ugly implement of nav bar

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
+    path: '/',
     title: 'Hello Koa 2!',
     message: 'Welcome to Koa 2'
   })
--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('upload', {
+    path: '/upload',
     title: "Upload Images",
     message: "Try uploading multiple files at a time."
   })
