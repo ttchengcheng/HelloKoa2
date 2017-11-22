7438943412724b967021158cb226a9db3c449d11
Tao Cheng <tchengim30@sina.com>
2017-11-22 10:31:18 +0800

    remove pugs

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-better-body')
 const logger = require('koa-logger')
 
-const views = require('./middlewares/views')
 const upload = require('./middlewares/upload')
 const router = require('./middlewares/router')
 const log = require('./middlewares/logger')
 const statics = require('./middlewares/statics')
-// const pageNotFound = require('./middlewares/page-not-found')
 
 // error handler
 onerror(app)
@@ -21,11 +19,9 @@ app.use(bodyparser())
   .use(json())
   .use(logger())
   .use(statics())
-  .use(views())
   .use(log())
   .use(upload())
   .use(router())
-  // .use(pageNotFound())
 
 // error-handling
 app.on('error', (err, ctx) => {
--- middlewares/views.js
+++
@@ -1,5 +0,0 @@
-const views = require('koa-views')
-const path = require('path')
-module.exports = () => {
-  return views(path.join(__dirname, '/../views'), {extension: 'pug'})
-}
